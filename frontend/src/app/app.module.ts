fd23ab7c57283292451ec719b5314c41a8b0f10c
Przemek-Gosik <pregosik322@gmail.com>
2023-05-14 17:09:43 +0200

    notes animation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,19 +4,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PostItNoteComponent } from './pages/home/post-it-note/post-it-note.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import {RouterModule} from "@angular/router";
-import {AppRoutingModule} from "./app-routing.module";
-import {HttpClientModule} from "@angular/common/http";
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MenuComponent } from './pages/menu/menu.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PostItNoteComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,9 +28,10 @@ import {MatIconModule} from "@angular/material/icon";
     HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
